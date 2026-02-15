c9de3076a14f956202ec101bcd41aefa951a3530
Kanan Sultanov <konansulx@gmail.com>
2026-02-15 22:37:41 +0400

    Adding load_dotenv() function and moving DATABASE_URL variable to .env file

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,11 +1,14 @@
+import os
 from typing import Generator
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 
-database_url = "postgresql+psycopg://llm-chatbot:llm-chatbot@localhost:5434/llm-chatbot"
+from dotenv import load_dotenv
+load_dotenv()
 
-engine = create_engine(database_url, echo = True)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL, echo = True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
