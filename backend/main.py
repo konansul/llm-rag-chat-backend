4df9fc733cd8f6f6104181b64d2d76df0459fa1b
Kanan Sultanov <konansulx@gmail.com>
2026-02-16 00:49:48 +0400

    Adding chat_title and documents routers to the main FastAPI file

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,21 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
-from backend.routers import auth, chats, messages
+from backend.routers import auth, chats, chat_title, messages, documents
 
 app = FastAPI(
-    title = "LLM and RAG Chat Platform API",
+    title = "LLM and RAG Chatbot API",
     description = "Production ready backend for LLM based chat bots with Retrieval-Augmented Generation",
-    version = "0.1.0"
+    version = "0.1.1"
 )
 
-app.add_middleware(
-    CORSMiddleware,
-)
+app.add_middleware(CORSMiddleware, )
+
 app.include_router(auth.router, tags = ["Users"])
 app.include_router(chats.router, tags = ["Chats"])
+app.include_router(chat_title.router, tags = ["Chat titles"])
 app.include_router(messages.router, tags = ["Messages"])
+app.include_router(documents.router, tags = ["Documents"])
 
 @app.get("/health")
 def health_check():
